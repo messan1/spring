8d020bab59738dfc2a875a6af61561262ce5149a
mexxxxe <mee@dd.fr>
2020-07-25 10:21:34 +0000

    adding conversation model without relation

--- src/main/java/com/constelis/constelis/Model/ContactConversation.java
+++ src/main/java/com/constelis/constelis/Model/ContactConversation.java
@@ -1,5 +1,28 @@
 package com.constelis.constelis.Model;
 
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter @Getter @NoArgsConstructor
 public class ContactConversation {
+
+    @Id
+    private final String id;
+    private final Date date;
+    private final String conversation;
+    private final String trigramme;
+
+    public ContactConversation(String id, Date date, String conversation, String trigramme) {
+        this.id = id;
+        this.date = date;
+        this.conversation = conversation;
+        this.trigramme = trigramme;
+    }
+    
     
 }
